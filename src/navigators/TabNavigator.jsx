d908407eef51961cb39bed77fe28ff837f780aa5
mahdi ghaffari <mahdi.ghaffari28@gmail.com>
2023-01-02 16:30:06 +0330

    feat: tabNavigator

--- src/navigators/TabNavigator.jsx
+++ src/navigators/TabNavigator.jsx
@@ -3,6 +3,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Account from "../screens/Account/Account";
 import Categories from "../screens/Categories/Categories";
 import Home from "../screens/Home/Home";
+import Cart from "../screens/Cart/Cart";
+import Bookmark from "../screens/Bookmark/Bookmark";
+import { Platform } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,29 +15,34 @@ const TabNavigator = () => {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
-          if (route.name === "Courses") {
-            iconName = focused ? "school" : "school";
-          } else if (route.name === "MyCourses") {
-            iconName = focused ? "message-video" : "message-video";
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Categories") {
+            iconName = focused ? "menu-open" : "menu";
           } else if (route.name === "Account") {
-            iconName = focused ? "account-circle" : "account-circle-outline";
+            iconName = focused ? "account-box" : "account-box-outline";
+          } else if (route.name === "Cart") {
+            iconName = focused ? "cart" : "cart-outline";
+          } else if (route.name === "Bookmark") {
+            iconName = focused ? "bookmark" : "bookmark-outline";
           }
           return (
             <MaterialCommunityIcons name={iconName} size={size} color={color} />
           );
         },
+
         tabBarStyle: {
-          backgroundColor: "#333533",
+          backgroundColor: "#ffffff",
           height: 70,
         },
         tabBarItemStyle: {
           marginHorizontal: 10,
           marginVertical: 10,
-          borderRadius: 10,
         },
         tabBarLabelStyle: { paddingBottom: 5 },
-        tabBarInactiveTintColor: "#eeeeee68",
-        tabBarActiveTintColor: "#fff",
+        tabBarInactiveTintColor: "#000",
+        tabBarActiveTintColor: "#0068c9",
+        // headerShown: Platform.OS === "ios" ? true : false,
         headerShown: false,
       })}
     >
@@ -44,14 +52,24 @@ const TabNavigator = () => {
         options={{ tabBarLabel: "اکانت کاربری" }}
       />
       <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{ tabBarLabel: "اکانت کاربری" }}
-      />
-      <Tab.Screen
         name="Categories"
         component={Categories}
-        options={{ tabBarLabel: "اکانت کاربری" }}
+        options={{ tabBarLabel: "دسته بندی" }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ tabBarLabel: "خانه" }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={Cart}
+        options={{ tabBarLabel: "سبد خرید" }}
+      />
+      <Tab.Screen
+        name="Bookmark"
+        component={Bookmark}
+        options={{ tabBarLabel: "علاقه مندی ها" }}
       />
     </Tab.Navigator>
   );
